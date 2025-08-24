f280fbd012b864ba30efe1ff6a5b9dce04f76322
Tharun Mukesh <tharunmukesh@Tharuns-Laptop.local>
2025-08-24 18:42:48 +0530

    gemini key updated

--- frontend/src/aiChatService.jsx
+++ frontend/src/aiChatService.jsx
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = import.meta.env.GEMINI_API_KEY
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent'
 
 class AIChatService {
